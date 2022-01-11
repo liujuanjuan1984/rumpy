6f1c22a0ba3c1112f79fe6d2299bc9703cc4826d
liujuanjuan1984 <liujuanjuan1984@gmail.com>
2022-01-11 15:57:39 +0800

    update: leave groups

--- examples/leave_groups/leave_groups.py
+++ examples/leave_groups/leave_groups.py
@@ -7,7 +7,6 @@ from rumpy import RumClient
 
 
 def main():
-
     # 初始化
     kwargs = {
         "appid": "peer",
@@ -17,13 +16,16 @@ def main():
     }
     bot = RumClient(**kwargs)
 
+    my_test_groups = ["测试hellorum", "测试whosays", "新增测试组", "nihao"]
+
     # 获取已经加入的所有组
     for gdata in bot.node.groups():
-        # 只离开自己自己创建的组
-        if gdata["owner_pubkey"] != bot.node.pubkey:
-            continue
-        # 这些是之前测试时大量创建的组，可离开
-        if gdata["group_name"] in ["测试hellorum", "测试whosays", "新增测试组", "nihao"]:
+        # 离开久未更新/或同步失败的组
+        # 只离开自己自己创建的组，这些是之前测试时大量创建的组，可离开
+        if (bot.trx._timestamp(gdata["last_updated"]) <= "2021-10-01") or (
+            gdata["owner_pubkey"] == gdata["user_pubkey"]
+            and gdata["group_name"] in my_test_groups
+        ):
             bot.group.leave(gdata["group_id"])
 
     print("如果开着桌面应用GUI，可以点击左上角的“重新加载”刷新页面。")
