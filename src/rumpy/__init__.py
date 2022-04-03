d718406c2c87b7ee90c5d0090549f0fbf6b6d8eb
liujuanjuan1984 <liujuanjuan1984@gmail.com>
2022-04-03 20:31:52 +0800

    version update to 0.0.8

--- src/rumpy/__init__.py
+++ src/rumpy/__init__.py
@@ -2,7 +2,7 @@ import logging
 from rumpy.client import RumClient
 from rumpy.client.module import *
 
-__version__ = "0.0.7"
+__version__ = "0.0.8"
 __author__ = "liujuanjuan1984"
 
 # Set default logging handler to avoid "No handler found" warnings.
