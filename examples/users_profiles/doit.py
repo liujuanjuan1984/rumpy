5d82235bb49892d6a903b1e478ef5a6b56069b9d
liujuanjuan1984 <liujuanjuan1984@gmail.com>
2022-05-07 20:09:21 +0800

    example profiles: support data_dir param

--- examples/users_profiles/doit.py
+++ examples/users_profiles/doit.py
@@ -1,7 +1,10 @@
+import os
 from users_profiles import UsersProfiles
 
 bot = UsersProfiles(port=58356)
 bot.group_id = "4e784292-6a65-471e-9f80-e91202e3358c"
 
 # give the file path or None to init it.
-bot.update(users_profiles_file=None)
+mydir = os.path.join(os.path.dirname(__file__), "data")
+
+bot.update(users_profiles_dir=mydir)
--- examples/users_profiles/users_profiles.py
+++ examples/users_profiles/users_profiles.py
@@ -1,15 +1,33 @@
 import datetime
+import os
 from rumpy import RumClient
 from officy import JsonFile, Stime
 
 
 class UsersProfiles(RumClient):
-    def update(self, users_profiles_file=None):
-        users_profiles_file = None or f"users_profiles_group_{self.group_id}.json"
+    def update(self, users_profiles_file=None, users_profiles_dir=None):
+
+        # check filepath
+        filename = f"users_profiles_group_{self.group_id}.json"
+        if users_profiles_dir:
+            users_profiles_file = os.path.join(users_profiles_dir, filename)
+        else:
+            users_profiles_file = users_profiles_file or filename
+
         users_data = JsonFile(users_profiles_file).read({})
 
+        # get new trxs from the flag: trx_id
         trx_id = users_data.get("trx_id")
+        from_tid = trx_id
         trxs = self.group.all_content_trxs(trx_id=trx_id)
+
+        # update trx_id: to record the progress to get new trxs
+        if len(trxs) > 0:
+            to_tid = trxs[-1]["TrxId"]
+        else:
+            to_tid = from_tid
+        users_data.update({"trx_id": to_tid})
+        print(datetime.datetime.now(), f"{len(trxs)} trxs is new. from {from_tid} to {to_tid}")
 
         profile_trxs = []
         for trx in trxs:
@@ -21,10 +39,6 @@ class UsersProfiles(RumClient):
 
         if "group_name" not in users_data:
             users_data["group_name"] = self.group.seed()["group_name"]
-
-        # trx_id: record the progress to get new trxs
-        if "trx_id" not in users_data:
-            users_data["trx_id"] = trxs[-1]["TrxId"] if len(trxs) > 0 else trx_id
 
         users = users_data.get("data") or {}
 
