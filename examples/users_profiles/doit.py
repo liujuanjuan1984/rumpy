139161cac134a11643c3f703ee7c57c8d6d7403f
liujuanjuan1984 <liujuanjuan1984@gmail.com>
2022-05-08 09:55:41 +0800

    update user profiles by data or file, for group or node; can choose profile_types as name,wallet image

--- examples/users_profiles/doit.py
+++ examples/users_profiles/doit.py
@@ -1,10 +1,18 @@
 import os
-from users_profiles import UsersProfiles
+from rumpy import RumClient
+from users_profiles import *
 
-bot = UsersProfiles(port=58356)
-bot.group_id = "4e784292-6a65-471e-9f80-e91202e3358c"
+client = RumClient(port=58356)
+client.group_id = "4e784292-6a65-471e-9f80-e91202e3358c"
 
 # give the file path or None to init it.
 mydir = os.path.join(os.path.dirname(__file__), "data")
 
-bot.update(users_profiles_dir=mydir)
+update_profiles(
+    client,
+    users_data=None,
+    users_profiles_file=None,
+    users_profiles_dir=mydir,
+    profile_types=("name", "wallet", "image"),
+)
+update_node_profiles(client, users_profiles_dir=mydir, profile_types=("name", "wallet", "image"))
--- examples/users_profiles/users_profiles.py
+++ examples/users_profiles/users_profiles.py
@@ -1,57 +1,72 @@
 import datetime
 import os
-from rumpy import RumClient
 from officy import JsonFile, Stime
+from typing import Dict, List
 
 
-class UsersProfiles(RumClient):
-    def update(self, users_profiles_file=None, users_profiles_dir=None):
+def update_group_profiles(client, users_data: Dict, profile_types=("name", "image", "wallet")):
 
-        # check filepath
-        filename = f"users_profiles_group_{self.group_id}.json"
-        if users_profiles_dir:
-            users_profiles_file = os.path.join(users_profiles_dir, filename)
-        else:
-            users_profiles_file = users_profiles_file or filename
+    # get new trxs from the trx_id
+    trx_id = users_data.get("trx_id")
+    trxs = client.group.all_content_trxs(trx_id=trx_id)
 
-        users_data = JsonFile(users_profiles_file).read({})
+    # update trx_id: to record the progress to get new trxs
+    if len(trxs) > 0:
+        to_tid = trxs[-1]["TrxId"]
+    else:
+        to_tid = trx_id
 
-        # get new trxs from the flag: trx_id
-        trx_id = users_data.get("trx_id")
-        from_tid = trx_id
-        trxs = self.group.all_content_trxs(trx_id=trx_id)
+    users_data.update(
+        {
+            "group_id": client.group_id,
+            "group_name": client.group.seed()["group_name"],
+            "trx_id": to_tid,
+            "update_at": str(datetime.datetime.now()),
+        }
+    )
 
-        # update trx_id: to record the progress to get new trxs
-        if len(trxs) > 0:
-            to_tid = trxs[-1]["TrxId"]
-        else:
-            to_tid = from_tid
-        users_data.update({"trx_id": to_tid})
-        print(datetime.datetime.now(), f"{len(trxs)} trxs is new. from {from_tid} to {to_tid}")
+    users = users_data.get("data") or {}
+    profile_trxs = [trx for trx in trxs if trx.get("TypeUrl") == "quorum.pb.Person"]
 
-        profile_trxs = []
-        for trx in trxs:
-            if trx.get("TypeUrl") == "quorum.pb.Person":
-                profile_trxs.append(trx)
-
-        if "group_id" not in users_data:
-            users_data["group_id"] = self.group_id
-
-        if "group_name" not in users_data:
-            users_data["group_name"] = self.group.seed()["group_name"]
-
-        users = users_data.get("data") or {}
-
-        for trx in profile_trxs:
-            pubkey = trx["Publisher"]
-            if pubkey not in users:
-                users[pubkey] = {"creat_at": str(datetime.datetime.now())}
-            for key in trx["Content"]:
+    for trx in profile_trxs:
+        pubkey = trx["Publisher"]
+        if pubkey not in users:
+            users[pubkey] = {"creat_at": str(datetime.datetime.now())}
+        for key in trx["Content"]:
+            if key in profile_types:
                 users[pubkey].update({key: trx["Content"][key]})
-            ts = str(Stime.ts2datetime(trx["TimeStamp"]))
-            users[pubkey].update({"update_at": ts})
+                ts = str(Stime.ts2datetime(trx["TimeStamp"]))
+                users[pubkey].update({"update_at": ts})
 
-        users_data.update({"data": users})
-        JsonFile(users_profiles_file).write(users_data)
-        print(datetime.datetime.now(), "update is done.", users_profiles_file)
-        return users
+    users_data.update({"data": users})
+    return users_data
+
+
+def update_profiles(
+    client,
+    users_data=None,
+    users_profiles_file=None,
+    users_profiles_dir=None,
+    profile_types=("name", "wallet", "image"),
+):
+
+    if users_data:
+        return update_group_profiles(client, users_data, profile_types)
+
+    filename = f"users_profiles_group_{client.group_id}.json"
+    if users_profiles_dir:
+        users_profiles_file = os.path.join(users_profiles_dir, filename)
+    else:
+        users_profiles_file = users_profiles_file or filename
+
+    users_data = JsonFile(users_profiles_file).read({})
+    users_data = update_group_profiles(client, users_data, profile_types)
+
+    JsonFile(users_profiles_file).write(users_data)
+    return users_data
+
+
+def update_node_profiles(client, users_profiles_dir, profile_types=("name", "wallet", "image")):
+    for gid in client.node.groups_id:
+        client.group_id = gid
+        update_profiles(client, users_profiles_dir=users_profiles_dir, profile_types=profile_types)
