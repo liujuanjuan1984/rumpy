d5bf9a0f454c4e522ae567494eb2a698e995163b
liujuanjuan1984 <liujuanjuan1984@gmail.com>
2022-05-20 13:28:23 +0800

    Bump version: 0.3.3 → 0.4.0

--- rumpy/__init__.py
+++ rumpy/__init__.py
@@ -2,7 +2,7 @@ import datetime
 import logging
 from rumpy.client import RumClient
 
-__version__ = "0.3.3"
+__version__ = "0.4.0"
 __author__ = "liujuanjuan1984"
 
 # Set default logging handler to avoid "No handler found" warnings.
