b846bf31a6bed4a4fb03edf28ea0c849cb557701
liujuanjuan1984 <liujuanjuan1984@gmail.com>
2022-08-23 10:36:24 +0800

    Bump version: 0.7.9 → 0.8.0

--- rumpy/__init__.py
+++ rumpy/__init__.py
@@ -3,7 +3,7 @@ import logging
 
 from rumpy.client import FullNode, HttpRequest, LightNode, MiniNode
 
-__version__ = "0.7.9"
+__version__ = "0.8.0"
 __author__ = "liujuanjuan1984"
 
 # Set default logging handler to avoid "No handler found" warnings.
