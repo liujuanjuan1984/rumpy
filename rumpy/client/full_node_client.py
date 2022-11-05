86a05a6e9cbcea634d7e46a9573aff8bdc1f3e9f
liujuanjuan1984 <liujuanjuan1984@gmail.com>
2022-11-06 00:51:09 +0800

    add param: jwt_token

--- rumpy/client/full_node_client.py
+++ rumpy/client/full_node_client.py
@@ -12,19 +12,29 @@ logger = logging.getLogger(__name__)
 class FullNode:
     _group_id = None
 
-    def __init__(self, protocol=None, port=None, host="127.0.0.1", crtfile=None):
-        if port is None:
-            port = os.getenv("RUM_PORT", 51194)
-        if crtfile is None:
-            local_crtfile = r"C:\Users\75801\AppData\Local\Programs\prs-atm-app\resources\quorum-bin\certs\server.crt"
-            crtfile = os.getenv("RUM_CRTFILE", local_crtfile)
-        if protocol is None:
-            if host in ["127.0.0.1", "localhost"]:
-                protocol = "http"
-            else:
-                protocol = "https"
-        _apis = ApiBaseURLS(protocol=protocol, port=port, host=host)
-        self.http = HttpRequest(api_base=_apis.FULL_NODE, crtfile=crtfile)
+    def __init__(
+        self,
+        protocol=None,
+        port=None,
+        host="127.0.0.1",
+        crtfile=None,
+        api_base=None,
+        jwt_token=None,
+    ):
+        if api_base is None:
+            if port is None:
+                port = os.getenv("RUM_PORT", 51194)
+            if crtfile is None:
+                local_crtfile = r"C:\Users\75801\AppData\Local\Programs\prs-atm-app\resources\quorum-bin\certs\server.crt"
+                crtfile = os.getenv("RUM_CRTFILE", local_crtfile)
+            if protocol is None:
+                if host in ["127.0.0.1", "localhost"]:
+                    protocol = "http"
+                else:
+                    protocol = "https"
+            api_base = ApiBaseURLS(protocol=protocol, port=port, host=host).FULL_NODE
+
+        self.http = HttpRequest(api_base=api_base, crtfile=crtfile, jwt_token=jwt_token)
         self.api = self.http.api = FullNodeAPI(self.http)
         self.paid = self.http.paid = PaidGroup(self.http)
         # TODO:paid其实也是 api 的一类，这个处理需要集中搞下
